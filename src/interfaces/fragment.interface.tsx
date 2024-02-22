e19ddf4fe0363b8ac19941f071316f2c45e24052
JoseWenned <118996462+JoseWenned@users.noreply.github.com>
2024-02-22 02:25:29 -0300

    feat: interface input

---
+++ src/interfaces/fragment.interface.tsx
@@ -0,0 +1,7 @@
+import { InputHTMLAttributes } from "react"; 
+import { FieldError } from "react-hook-form"; 
+
+export interface IInputProps extends InputHTMLAttributes<HTMLInputElement> {
+    label?: string
+    error?: FieldError
+}
